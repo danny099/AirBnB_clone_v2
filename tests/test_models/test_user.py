c62536377d37044c50c81f23a9b730e4c0e82cf7
Alejo Lopez <alejolo311@gmail.com>
2020-03-31 13:53:06 -0500

    user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -62,6 +62,8 @@ class TestUser(unittest.TestCase):
         self.assertEqual(type(self.user.first_name), str)
         self.assertEqual(type(self.user.first_name), str)
 
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == "db",
+                     "can't run")
     def test_save_User(self):
         """test if the save works"""
         self.user.save()
