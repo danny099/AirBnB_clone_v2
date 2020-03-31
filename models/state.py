e5cf8be8d21e7a2f6e9eec3f7054e1b743b3848f
Alejo Lopez <alejolo311@gmail.com>
2020-03-31 10:10:54 -0500

    updating city and state

--- models/state.py
+++ models/state.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.city import City
+import os
 
 
 class State(BaseModel):
@@ -8,4 +12,20 @@ class State(BaseModel):
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128),
+                  nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'fs':
+        @property
+        def cities(self):
+            _cities = []
+            for _id, city in models.storage.all(City).items():
+                if self.id == city.state_id:
+                    _cities.append(city)
+            return _cities
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete-orphan")
